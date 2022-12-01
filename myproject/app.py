dc757093e4ff53d41baf3e724e0f260f250be272
peetank@fedora <peetank@github.com>
2022-12-02 01:17:36 +0530

    add static/styles.css

--- myproject/app.py
+++ myproject/app.py
@@ -1,27 +1,27 @@
-from flask import Flask, request, render_template, url_for # importing Flask and other modules
+from flask import Flask, request, render_template # importing Flask and other modules
+
 import random #for generating random integers
  
 # Flask constructor
 app = Flask(__name__)  
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
  
-@app.route('/', methods =["GET", "POST"])
-def email_slicer():
-    if request.method == "POST": 
+@app.route('/', methods =["GET","POST"])
+def index():
+    if request.method == "POST" : 
        email = request.form.get("email")  # getting input with name = email in HTML form
        email = email.strip()  #getting rid of unnecessary spaces
-       email1 = email.split("@")[0]  #splitting the string at '@" symbol 
-       domain = email.split("@")[1]  #assigning domain address to a variable
-       if email == "":
-       		user_name = "NO Input!"
-       else :
-                user_name = email1[0:5]+ str(random.randint(0,10)) #adding random generated integer
+       email1 = ""
+       domain = ""
+       if email.count('@')==1:
+         email1 = email.split("@")[0]  #splitting the string at '@" symbol 
+         domain = email.split("@")[1]  #assigning domain address to a variable
+         user_name = email1[0:5]+ str(random.randint(0,10)) #adding random generated integer
+       else : 
+           user_name = "Error! No/Wrong Input!"
  
-    return render_template('index.html',result1 = email1, result2 = domain, result3 = user_name )
- 
+       return render_template('index.html',result1 = email1, result2 = domain, result3 = user_name )
+    else : 
+       return render_template('index.html')
+
 if __name__=='__main__':
    app.run()
